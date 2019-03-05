4dbea61f5562a18b80e167c178411816c60ce4af
Ali Nazari <anap1899@gmail.com>
2019-03-05 12:18:09 +0330

    add mb_json_encode

--- src/helpers.php
+++ src/helpers.php
@@ -71,3 +71,20 @@ if (!function_exists('to_gregorian')) {
         return app('helper')->toGregorian($jDate);
     }
 }
+
+if (! function_exists('mb_json_encode')) {
+    /**
+     * json_encode() for multibyte characters
+     *
+     * @param mixed
+     *
+     * @return null|string
+     */
+    function mb_json_encode($input) {
+        return preg_replace_callback('/\\\\u([0-9a-zA-Z]{4})/', function ($matches) {
+            return mb_convert_encoding(pack('H*',$matches[1]), 'UTF-8','UTF-16');
+        },
+            json_encode($input, JSON_UNESCAPED_UNICODE)
+        );
+    }
+}
